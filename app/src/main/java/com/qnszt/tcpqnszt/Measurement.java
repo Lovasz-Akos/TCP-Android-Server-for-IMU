b345725c9f7be2b66f0f1d869d781593f4e9b6b6
Dombi Tibor <dombidav@gmail.com>
2020-07-16 14:17:35 +0200

    Added mapping for delay

--- app/src/main/java/com/qnszt/tcpqnszt/Measurement.java
+++ app/src/main/java/com/qnszt/tcpqnszt/Measurement.java
@@ -33,13 +33,19 @@ public class Measurement {
         return delay;
     }
 
-    public void setDelay(int delay) {
-        this.delay = delay; //map(delay, 50, 100, 20, 10).intValue();
+    public void setDelay(float hertz) {
+        //this.delay = (int)Math.floor(calculateDelay(hertz)); //map(delay, 50, 100, 20, 10).intValue();
+        this.delay = map(hertz, 50,100, 20,10).intValue();
     }
 
     Float map(float x, float in_min, float in_max, float out_min, float out_max)
     {
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+
+    double calculateDelay(float x){
+        //return (0.000344431 * Math.pow(x, 4)) - (0.0354434 * Math.pow(x, 3)) + (1.41469 * Math.pow(x, 2)) - (27.3635 * x) + 258.165;
+        return (-0.0113332 * Math.pow(x, 3)) + (0.803327 * Math.pow(x, 2)) - (20.7332 * x) + 232.333;
     }
 
     public void setDelay(String delay) {
