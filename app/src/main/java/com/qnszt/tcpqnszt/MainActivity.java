35d5ce3fed5d7614983badc9a1a417f0079ff179
Lovász Ákos <akos4x44@gmail.com>
2020-07-21 13:45:28 +0200

    added method to free up memory when UI is hidden or ran out of memory

--- app/src/main/java/com/qnszt/tcpqnszt/MainActivity.java
+++ app/src/main/java/com/qnszt/tcpqnszt/MainActivity.java
@@ -1,6 +1,7 @@
 package com.qnszt.tcpqnszt;
 
 import android.annotation.SuppressLint;
+import android.content.ComponentCallbacks2;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -64,8 +65,63 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    public void onTrimMemory(int level) {
 
-    public void startMeasurementClicked() {
+        // Determine which lifecycle or system event was raised.
+        switch (level) {
+
+            case ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN:
+
+                /*
+                   Release any UI objects that currently hold memory.
+
+                   The user interface has moved to the background.
+                */
+
+                break;
+
+            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_MODERATE:
+            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_LOW:
+            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_CRITICAL:
+
+                /*
+                   Release any memory that your app doesn't need to run.
+
+                   The device is running low on memory while the app is running.
+                   The event raised indicates the severity of the memory-related event.
+                   If the event is TRIM_MEMORY_RUNNING_CRITICAL, then the system will
+                   begin killing background processes.
+                */
+
+                break;
+
+            case ComponentCallbacks2.TRIM_MEMORY_BACKGROUND:
+            case ComponentCallbacks2.TRIM_MEMORY_MODERATE:
+            case ComponentCallbacks2.TRIM_MEMORY_COMPLETE:
+
+                /*
+                   Release as much memory as the process can.
+
+                   The app is on the LRU list and the system is running low on memory.
+                   The event raised indicates where the app sits within the LRU list.
+                   If the event is TRIM_MEMORY_COMPLETE, the process will be one of
+                   the first to be terminated.
+                */
+
+                break;
+
+            default:
+                /*
+                  Release any non-critical data structures.
+
+                  The app received an unrecognized memory level value
+                  from the system. Treat this as a generic low-memory message.
+                */
+                break;
+        }
+
+
+        public void startMeasurementClicked() {
         Measurement measurement = new Measurement();
         measurement.setName(((TextView) findViewById(R.id.txt_measurementName)).getText().toString());
         measurement.setDuration(((TextView) findViewById(R.id.txt_measurementDuration)).getText().toString());
